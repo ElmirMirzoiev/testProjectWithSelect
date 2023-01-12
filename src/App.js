7f625a92ebd4e498592299498ed1fe417f3a6f71
Elmir Mirzoiev <elmir.mirzoiev@outlook.com>
2023-01-12 18:02:30 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import "./App.css"
-import axios from "axios"
-import MySelect from "./components/MySelect"
-import React, {useEffect, useRef, useState} from "react"
+import React, {useEffect, useRef, useState} from 'react'
+import axios from 'axios'
+import './App.css'
+import AppContainer from './components/AppContainer'
 
 function App() {
 
-    const src = "http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5"
+    const src = 'http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5'
     const shouldLoad = useRef(true)
-    const [arr, setArr] = useState([])
+    const [data, setData] = useState([])
     useEffect(() => {
         if (shouldLoad.current) {
             shouldLoad.current = false
-            axios.get(src).then(res => setArr(res.data))
+            axios.get(src).then(res => setData(res.data))
         }
     }, [])
 
     return (
-        <div className='App'>
-            <div className='board1'>
-                <MySelect setArr={setArr} arr={arr}/>
+        <div className="App">
+            <div className="board1">
+                <AppContainer data={data} setData={setData}/>
             </div>
-            <div className='board2'>
-                <MySelect setArr={setArr} arr={arr}/>
+            <div className="board2">
+                <AppContainer data={data} setData={setData}/>
             </div>
         </div>
     )
